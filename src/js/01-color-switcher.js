606dc4f846e3763f254f1891a1f32dae9619a790
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2024-06-10 16:37:49 +0300

    Colorswitcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const element = document.body;
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+buttonStart.addEventListener("click", () => {
+    buttonStart.disabled = 'true';
+  timerId = setInterval(() => {
+      element.style.backgroundColor=getRandomHexColor();
+  }, 1000);
+});
+
+
+buttonStop.addEventListener("click", () => {
+    clearInterval(timerId);
+     buttonStart.disabled = '';
+    
+});
