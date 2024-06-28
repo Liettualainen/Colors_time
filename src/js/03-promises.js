ad230617e898fce282804848fe658b769677a075
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2024-06-28 17:40:59 +0300

    promisegenerator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const promiseAmount = form.elements.amount;
+const promiseDelay = form.elements.delay;
+const promiseStep = form.elements.step;
+const submitButton = form.querySelector('[type="submit"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let delay = Number(promiseDelay.value);
+  let stepDelay = Number(promiseStep.value);
+  let amount = Number(promiseAmount.value);
+  
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, delay+i*stepDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+         {
+            timeout: 3000, useIcon: false, width: '240px',
+         },
+ )
+  })
+      .catch(({ position, delay }) => {
+       Notiflix.Notify.failure(
+      `❌ Rejected promise ${position} in ${delay}ms`, {
+     timeout:  3000, useIcon: false, width: '240px',},)
+  });
+  }
+  form.reset();
+});
